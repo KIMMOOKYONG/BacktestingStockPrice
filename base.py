66b3353f7877d403e8894a04ec9c0a612477a952
KIMMOOKYONG <dharma6872@gamil.com>
2022-04-27 06:39:37 +0000

    백테스팅업로드

--- base.py
+++ base.py
@@ -28,14 +28,15 @@ plt.rcParams.update({'font.size': 12})
 warnings.simplefilter(action="ignore", category=FutureWarning)
 warnings.filterwarnings("ignore")
 
-class Base():
+class MLBacktraderBase():
     def __init__(self, ticker, start, end):
 
-        self.ticker = ticker
-        self.start = start
-        self.end = end
-        self.data = None
-        self.cols_bin = []
+        self.ticker = ticker # 종목코드
+        self.start = start # 시작일
+        self.end = end # 종료일
+        self.data = None # 데이터
+        self.cols = [] # 주가 변화율 계산 칼럼명
+        self.cols_bin = [] # 주가 변화율 계산 바이너리(0,1) 칼럼명
         self.strategy_rtn = []
 
         self.models = {
@@ -46,65 +47,79 @@ class Base():
             "MLP": MLPClassifier(),
         }
 
-    def download_data(self, cols="Adj Close"):
+    def download_data(self, cols=["Adj Close"]):
+        """
+        yfinance에서 데이터 파일 다운로드하는 함수
+        cols: 다운로드할 칼럼명 목록
+        """
 
-        data = yf.download(self.ticker, progress=True, actions=True, start=self.start, end=self.end)[",".join(cols)]
-        data = pd.DataFrame(data)
-        data.rename(columns={"Adj Close":self.ticker}, inplace=True)
+        self.data = yf.download(self.ticker, progress=True, actions=True, start=self.start, end=self.end)[",".join(cols)]
+        self.data = pd.DataFrame(data)
+        self.data.rename(columns={"Adj Close":self.ticker}, inplace=True)
 
-        data["returns"] = np.log(data / data.shift(1))
-        data.dropna(inplace=True)
-        data["direction"] = np.sign(data["returns"]).astype(int)
+        self.data["returns"] = np.log(self.data / self.data.shift(1))
+        self.data.dropna(inplace=True)
+        self.data["direction"] = np.sign(self.data["returns"]).astype(int)
 
-        return data
-
-    def save_price_daily_returns_chart_image(self, data, filename="price_daily_returns.png"):
+    def save_price_daily_returns_chart_image(self, data, filename="price_daily_returns.png", image_to_file=False):
+        """
+        일변 주가 변동 및 가격 변화율 시각화하는 함수
+        data: 주가 데이터
+        filename: 저장할 파일명
+        image_to_file: 차트 이미지를 파일로 저장할지 여부, 기본값 False
+        """
 
         fig, ax = plt.subplots(2, 1, sharex=True, figsize=(12,6))
-        ax[0].plot(self.data[self.ticker], label=f"{self.ticker} Adj Close")
+        ax[0].plot(data[self.ticker], label=f"{self.ticker} Adj Close")
         ax[0].set(title=f"{self.ticker} Closing Price", ylabel="Price")
         ax[0].grid(True)
         ax[0].legend()
 
-        ax[1].plot(self.data["returns"], label="Daily Returns")
+        ax[1].plot(data["returns"], label="Daily Returns")
         ax[1].set(title=f"{self.ticker} Daily Returns", ylabel="Returns")
         ax[1].grid(True)
         ax[1].legend()
 
         plt.tight_layout()
-        plt.savefig(filename, dpi=300)
 
-    def create_lags_data(self):
+        if image_to_file:
+            plt.savefig(filename, dpi=300)
 
-        lags = [1,2,3,4,5]
-        cols = []
+    def create_lags_data(self, lags=[1,2,3,4,5]):
+        """
+        기간별 주가 변화율을 계산하는 함수
+        lags: 주가 변화율을 계산할 기간 설정
+        """
+
+        lags = lags
+        self.cols = []
         for lag in lags:
             col = f"rtn_lag{lag}"
             self.data[col] = self.data["returns"].shift(lag)
-            cols.append(col)
+            self.cols.append(col)
             self.data.dropna(inplace=True)
 
     def create_bins_data(self, bins=[0]):
 
         self.cols_bin = []
-        for col in cols:
+        for col in self.cols:
             col_bin = col + "_bin"
             self.data[col_bin] = np.digitize(self.data[col], bins=bins)
             self.cols_bin.append(col_bin)
 
     def fit_models(self):
 
-        mfit = {model: models[model].fit(self.data[cols_bin], self.data["direction"]) for model in models.keys()}
+        mfit = {model: self.models[model].fit(self.data[self.cols_bin], self.data["direction"]) for model in self.models.keys()}
 
     def derive_positions(self):
 
-        for model in models.keys():
-            self.data["pos_" + model] = models[model].predict(self.data[self.cols_bin])
+        for model in self.models.keys():
+            self.data["pos_" + model] = self.models[model].predict(self.data[self.cols_bin])
 
     def evaluate_strats(self):
 
         self.strategy_rtn = []
-        for model in models.keys():
+        for model in self.models.keys():
             col = "strategy_" + model
             self.data[col] = self.data["pos_" + model] * self.data["returns"]
             self.strategy_rtn.append(col)
